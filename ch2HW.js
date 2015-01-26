19a6acc0f527a329a77f54a07a83907f45fedd35
Craig <cav9871@saintjoe.edu>
2015-01-26 12:20:19 -0500

    I added the comments and changed the name because I miss labeled it

---
+++ ch2HW.js
@@ -0,0 +1,9 @@
+// Craig Austgen
+// chapter 2 hw
+// starts by putting one hash mark and for each new line you add another hash
+// until you reach 8 hash marks
+
+
+for (var line = "#"; line.length < 8; line += '#' ) {
+	print(line);
+}
